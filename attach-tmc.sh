a2ae02ef036bb9fb14cd0fd5e95799608751c3e4
warroyo <warroyo7199008@gmail.com>
2020-12-04 10:41:20 -0700

    no configure

--- attach-tmc.sh
+++ attach-tmc.sh
@@ -3,6 +3,6 @@
 echo "attaching ${CLUSTER_NAME} to TMC"
 
 export TMC_API_TOKEN=$1
-tmc login --no-configure
+tmc login --no-configure --name cs-gitops
 tmc cluster attach -g vra-gitops -n ${CLUSTER_NAME}
-kubectl apply -f k8s-attach-manifest.yaml
+kubectl apply -f ./k8s-attach-manifest.yaml
