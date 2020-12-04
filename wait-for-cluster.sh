68dbcbb4dcd9682afa83da39b2538137bda4c54f
warroyo <warroyo7199008@gmail.com>
2020-12-04 08:40:08 -0700

    fix script

--- wait-for-cluster.sh
+++ wait-for-cluster.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 NAMESPACE=$(kubectl apply -f cluster.yml --dry-run -o=jsonpath='{.metadata.namespace}')
-currentstatus=$(kubectl get tkc -o=jsonpath='{.items[0].status.phase}' -n ${NAMESPACE})
+CLUSTER_NAME=$(kubectl apply -f cluster.yml --dry-run -o=jsonpath='{.metadata.name}')
+currentstatus=$(kubectl get tkc ${CLUSTER_NAME} -o=jsonpath='{.items[0].status.phase}' -n ${NAMESPACE})
 statusdone="running"
 while [ "$currentstatus" != "$statusdone" ]
 do
   echo "Still Building Cluster"
   sleep 20
-  currentstatus=$(kubectl get tkc -o=jsonpath='{.items[*].status.phase}' -n ${NAMESPACE})
+  currentstatus=$(kubectl get tkc ${CLUSTER_NAME} -o=jsonpath='{.items[*].status.phase}' -n ${NAMESPACE})
   echo "current status: ${currentstatus}"
 done
 
