9ec6e0c85f5a78c7f0c92eea7e7b2428f4cedbb1
warroyo <warroyo7199008@gmail.com>
2020-12-03 18:12:01 -0700

    test

--- wait-for-cluster.sh
+++ wait-for-cluster.sh
@@ -3,11 +3,12 @@
 NAMESPACE=$(kubectl apply -f cluster.yml --dry-run -o=jsonpath='{.metadata.namespace}')
 currentstatus=$(kubectl get tkc -o=jsonpath='{.items[0].status.phase}' -n ${NAMESPACE})
 statusdone="running"
-while [ $currentstatus != $statusdone ]
+while [ "$currentstatus" != "$statusdone" ]
 do
   echo "Still Building Cluster"
   sleep 20
   currentstatus=$(kubectl get tkc -o=jsonpath='{.items[*].status.phase}' -n ${NAMESPACE})
+  echo "current status: ${currentstatus}"
 done
 
 echo "Cluster Build Complete"
