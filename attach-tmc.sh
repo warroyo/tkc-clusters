13bea13ecaecfc37a95fb00f53c75418a2468da6
warroyo <warroyo7199008@gmail.com>
2020-12-15 18:11:37 -0700

    add cluster check

--- attach-tmc.sh
+++ attach-tmc.sh
@@ -4,5 +4,12 @@ echo "attaching ${CLUSTER_NAME} to TMC"
 
 export TMC_API_TOKEN=$1
 tmc login --no-configure --name cs-gitops
-tmc cluster attach -g vra-gitops -n ${CLUSTER_NAME}
-kubectl apply -f ./k8s-attach-manifest.yaml
+
+if tmc cluster list | grep -q "${CLUSTER_NAME}"; then
+    echo "Cluster exists"
+    exit 0
+else
+    tmc cluster attach -g vra-gitops -n ${CLUSTER_NAME}
+    kubectl apply -f ./k8s-attach-manifest.yaml
+fi
+
